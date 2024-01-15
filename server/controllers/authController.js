b77ea4428af88b8cfe4e857361b746d2dfda9069
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2024-01-15 21:51:49 +0500

    "update signup api"

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,51 +1,143 @@
-const userModel = require("../model/userSchema")
-const bcrypt = require("bcryptjs")
-const jwt = require('jsonwebtoken')
+// const userModel = require("../model/userSchema")
+// const bcrypt = require("bcryptjs")
+// const jwt = require('jsonwebtoken')
+// const asyncHandler = require("express-async-handler");
 
 
-const SignupController = async (req,res)=>{
-    try {
-        const body = req.body;
-        console.log(body)
+// const SignupController = async (req,res)=>{
+//     try {
+//         const body = req.body;
+//         console.log(body)
 
-        const {fullName , email, password} = body
-        if(!fullName|| !email|| !password){
-            return res.status(400).send({message: "required filed are missing"})
-        }
-        const hashpass = await bcrypt.hash(password,5)
-        const objToSend = {
-            full_Name: fullName,
-            email: email,
-            password: password
-        }
+//         const {fullName , email, password} = body
+//         if(!fullName|| !email|| !password){
+//             return res.status(400).send({message: "required filed are missing"})
+//         }
+//         const hashpass = await bcrypt.hash(password,5)
+//         const objToSend = {
+//             full_Name: fullName,
+//             email: email,
+//             password: password
+//         }
 
-        const emailExist = await userModel.findOne({email});
-        if(emailExist){
-            return res.status(400).send({message: "email already exist"})
-        }
+//         const emailExist = await userModel.findOne({email});
+//         if(emailExist){
+//             return res.status(400).send({message: "email already exist"})
+//         }
 
-        const userSave = await userModel.create(objToSend);
+//         const userSave = await userModel.create(objToSend);
 
-        res.status(200).send({status: true, message: "user sign in successfully", data:userSave})
+//         res.status(200).send({status: true, message: "user sign in successfully", data:userSave})
 
 
         
-    } catch (error) {
-        res.json({
-            status: false,
-            message: error.message
-        })
-    }
+//     } catch (error) {
+//         res.json({
+//             status: false,
+//             message: error.message
+//         })
+//     }
 
-}
+// }
 
-//get all user api
-const getAllUser = async (req,res)=>{
-    const keyword = req.query
-    console.log(keyword);
-}
+// //get all user api
+// const getAllUser = asyncHandler(async (req, res) => {
+//   const keyword = req.query.search
+//     ? {
+//         $or: [
+//           { name: { $regex: req.query.search, $options: "i" } },
+//           { email: { $regex: req.query.search, $options: "i" } },
+//         ],
+//       }
+//     : {};
 
-module.exports ={
-    SignupController,
-    getAllUser
-}
+//   const users = await userModel.find(keyword)
+// //   res.send(users);
+// });
+
+// module.exports ={
+//     SignupController,
+//     getAllUser
+// }
+
+const asyncHandler = require("express-async-handler");
+const User = require("../model/userSchema");
+const generateToken = require("../config/generateToken");
+
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword)
+  res.send(users);
+});
+
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    // pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    //   isAdmin: user.isAdmin,
+    //   pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+
+//@description     Auth the user
+//@route           POST /api/users/login
+//@access          Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    //   isAdmin: user.isAdmin,
+    //   pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { allUsers, registerUser, authUser };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { SignupController, getAllUser } = require('../controllers/authController');
+const { registerUser, allUsers } = require('../controllers/authController');
 const app = express();
 const router = express.Router();
 
 
-router.post("/api/signup",SignupController)
-router.get("/api/getalluser",getAllUser)
+router.post("/api/signup",registerUser)
+router.get("/api/getalluser",allUsers)
 
 
 
